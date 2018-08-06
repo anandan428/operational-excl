d41951bb1a37db33a166f544aab6f3903a8b2c33
Nandan A <nandan.a@volvo.com>
2018-08-06 14:14:19 +0530

    adding menu

---
+++ src/components/menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './menu.css';
+
+const Menu = props => (
+    <div className="menu-container">
+        <div style={{position: 'sticky', top: '72px'}}>
+            <nav>
+                
+            </nav>
+        </div>
+    </div>
+)
+
+
+export default Menu;
